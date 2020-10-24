27a690a7ed67bd35a904ae94d8a5e8e64a137b5a
Jenni <justlittleoldmejenn@gmail.com>
2020-10-24 13:52:34 -0700

    Works, but no firebase

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -5,14 +5,6 @@
     var App = window.App || {};
     var $ = window.jQuery;
 
-    // initalize firebase
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
-    var firestore = firebase.firestore();
-    const docRef = firestore.collection("coffee");
-   
-    var coffeeOrder = docRef.where('emailAddress' , '==', key);
-
     function RemoteDataStore(url) {
         if (!url) {
             throw new Error('No remote URL supplied.');
@@ -21,27 +13,13 @@
         this.serverUrl = url;
     }
 
-    coffeeOrder.get().then(function(querySnapshot){
-        console.log(querySnapshot)
-        if (querySnapshot.empty){
-            console.log("not found");
-            docRef.add(val).then(function(docRef){
-
-            })
-        }
-    })
-
-
-
-   RemoteDataStore.prototype.add = function (key, val) {
-      $.post(this.serverUrl, val, function (serverResponse) {
+    RemoteDataStore.prototype.add = function (key, val) {
+        $.post(this.serverUrl, val, function (serverResponse) {
             console.log(serverResponse);
         });
-   };
+    };
 
-   
-
-     RemoteDataStore.prototype.getAll = function (cb) {
+    RemoteDataStore.prototype.getAll = function (cb) {
         $.get(this.serverUrl, function (serverResponse) {
             console.log(serverResponse);
             cb(serverResponse);
@@ -55,7 +33,6 @@
         });
     };
 
-        // if problems remove ajax calls
     RemoteDataStore.prototype.remove = function (key) {
         $.ajax(this.serverUrl + '/' + key, {
             type: 'DELETE'
